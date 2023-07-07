ad6ce737e58d38ff3879c723b9d0e4fbce8f059c
George Philbrick <gphilbri@gmail.com>
2023-07-07 18:49:30 -0400

    Add worldmap

--- Assets/Scripts/TileBehavior.cs
+++ Assets/Scripts/TileBehavior.cs
@@ -5,35 +5,55 @@ using UnityEngine.Tilemaps;
 
 public class TileBehavior : MonoBehaviour
 {
-    enum PathAble
+    static Vector3Int [] NEIGHBOR_COORDS = {
+        Vector3Int.left,
+        Vector3Int.left + Vector3Int.up,
+        Vector3Int.up,
+        Vector3Int.up + Vector3Int.right,
+        Vector3Int.right,
+        Vector3Int.right + Vector3Int.down,
+        Vector3Int.down,
+        Vector3Int.down + Vector3Int.left,
+    };
+
+    public enum PathAble
     {
         BLOCKS_MOVEMENT = 0,
         ALLOWS_MOVEMENT = 1,
     }
-    PathAble CanPath;
-    IsometricRuleTile ThisTile;
-    Vector3Int IsoCoordinates;
-    Vector3 WorldCoordinates;
-
-    void Awake()
-    {
-        Transform transform = GetComponent<Transform>();
-        WorldCoordinates = transform.position;
-        Grid worldGrid = GetComponentInParent<Grid>();
-        IsoCoordinates = worldGrid.WorldToCell(WorldCoordinates);
-        Tilemap tilemap = GetComponentInParent<Tilemap>();
-        ThisTile = tilemap.GetTile<IsometricRuleTile>(IsoCoordinates);
-    }
-
+    public PathAble CanPath;
+    [HideInInspector] public IsometricRuleTile ThisTile;
+    [HideInInspector] public Vector3Int IsoCoordinates;
+    [HideInInspector] public Vector3 WorldCoordinates;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Transform transform = GetComponent<Transform>();
+        WorldCoordinates = transform.position;
+        Tilemap tilemap = GetComponentInParent<Tilemap>();
+        IsoCoordinates = tilemap.WorldToCell(WorldCoordinates);
+        ThisTile = tilemap.GetTile<IsometricRuleTile>(IsoCoordinates);
+        WorldMap.instance.PublishTile(IsoCoordinates, gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public bool IsUpper() {
+        return true; // TODO What is upper
+    }
+
+    public List<TileBehavior> GetNeighbors() {
+        List<TileBehavior> neighbors = new List<TileBehavior>();
+        foreach (Vector3Int coord in NEIGHBOR_COORDS) {
+            TileBehavior tile = WorldMap.instance.GetTopTile(coord);
+            if (tile) {
+                neighbors.Add(tile);
+            }
+        }
+        return neighbors;
     }
 }
---
+++ Assets/Scripts/WorldMap.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMap : MonoBehaviour
+{
+    public struct WorldTile {
+        public bool present_upper;
+        public bool present_lower;
+        public TileBehavior upper_tile;
+        public TileBehavior lower_tile;
+    }
+
+    public WorldTile [,] map;
+    public static WorldMap instance;
+
+    void Awake()
+    {
+        instance = this;
+        map = new WorldTile[100, 100];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PublishTile(Vector3Int coords, GameObject obj) {
+        TileBehavior tile_behavior = obj.GetComponent<TileBehavior>();
+        if (tile_behavior.IsUpper()) {
+            map[coords.x + 50, coords.y + 50].present_upper = true;
+            map[coords.x + 50, coords.y + 50].upper_tile = tile_behavior;
+        } else {
+            map[coords.x + 50, coords.y + 50].present_lower = true;
+            map[coords.x + 50, coords.y + 50].lower_tile = tile_behavior;
+        }
+    }
+
+    public void UnPublishTile(Vector3Int coords, GameObject obj) {
+        WorldTile tile = map[coords.x + 50, coords.y + 50];
+        if (tile.upper_tile.gameObject == obj) {
+            tile.present_upper = false;
+            tile.upper_tile = null;
+        } else if (tile.lower_tile.gameObject == obj) {
+            tile.present_lower = false;
+            tile.lower_tile = null;
+        }
+    }
+
+    public TileBehavior GetTopTile(Vector3Int coords) {
+        WorldTile tile = map[coords.x + 50, coords.y + 50];
+        if (tile.present_upper) {
+            return tile.upper_tile;
+        } else if (tile.present_lower){
+            return tile.lower_tile;
+        }
+        return null;
+    }
+}
