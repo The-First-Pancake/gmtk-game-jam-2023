7a6f124583d3702e6998d6e4530e0c60ba174bc6
Declan Morrison <declan@morrison-okeefe.com>
2023-07-08 11:26:40 -0400

    Created Main Menu:

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    Vector2 StartPos;
+
+    [SerializeField] int moveModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pz = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        float posX = Mathf.Lerp(transform.position.x, StartPos.x + (pz.x * moveModifier), 2f * Time.deltaTime);
+        float posY = Mathf.Lerp(transform.position.y, StartPos.y + (pz.y * moveModifier), 2f * Time.deltaTime);
+
+        transform.position = new Vector3(
+            posX,
+            StartPos.y,
+            0
+        );
+
+    }
+}
